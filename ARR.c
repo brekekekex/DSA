f0ff6a876fc38d6cd34f40b4a562eafef12dc9b3
brekekekex <ynakamot@u.rochester.edu>
2020-05-18 01:39:31 -0400

    more

--- ARR.c
+++ ARR.c
@@ -67,18 +67,44 @@ struct ARR* prepend(int d, struct ARR* a){
 }
 
 struct ARR* join(struct ARR* a, struct ARR* b){
-    
-
+    int l = (a->length) + (b->length);
+    int* arr = malloc(sizeof(int)*l);
+    arrFill(arr, a->head, a->length);
+    arrFill(arr + (a->length), b->head, b->length);
+    free(b->head);
+    b->length = 0;
+    free(b);
+    free(a->head);
+    a->head = arr;
+    a->length = l;
+    return a;
 }
 
+int locate(int d, struct ARR* a){
+    for(int i = 0; i < a->length; i++){
+        if((a->head)[i] == d){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int contains(int d, struct ARR* a){
+    return (locate(d, a) != -1); 
+}
+
+    
 int main(void){
     int alpha[] = {1, 2, 3, 4, 5};
     int beta[] = {1, 2, 3, 4, 6};
 
     struct ARR* a = fromArray(alpha, 5);
+    struct ARR* b = fromArray(beta, 5);
+
+    a = join(a,b);
     printARR(a);
-    a = prepend(69, a);
-    printARR(a);
+    
+    printf("%d\n", contains(609, a));
     return 0;
 
 }
